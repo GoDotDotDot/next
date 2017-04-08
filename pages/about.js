86536812da15defd7d5222c36ec3501d6bbbe572
GoDotDotDot <chukui0202@gmail.com>
2017-04-09 01:34:33 +0800

    secend

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
 import Header from '../components/Header'
-
+import style from './about.css'
+import style2 from './about2.css'
 export default () => (
   <div>
     <Header />
-    <p>This is the ABOUT page.</p>
+    <p className={style.about_p}>This is the ABOUT page.</p>
+    <span className={style2.abt2}>{JSON.stringify(style)}</span>
   </div>
 )
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
-import Header from '../components/Header'
-import {Button} from 'antd'
+import NavHeader from 'next/head'
+import {Button, Layout} from 'antd'
+const { Header, Content, Footer } = Layout
 export default () => (
-  <div>
-    <Header />
-    <p>This is the HOME page</p>
-    <Button>antd Button</Button>
-  </div>
+  <Layout>
+    <NavHeader>
+      <title>My page title</title>
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <link rel='stylesheet' href='./static/css/commons.css' />
+    </NavHeader>
+    <Header>header</Header>
+    <Layout>
+      <Content>main content
+        <Button>btn</Button>
+      </Content>
+    </Layout>
+    <Footer>footer</Footer>
+  </Layout>
 )
